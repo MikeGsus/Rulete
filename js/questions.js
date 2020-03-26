a569d9fad4b65997c4dee4079fa81b873823c2ae
MikeGsus <mike08ahjhotmail.com>
2020-03-25 23:07:31 -0600

    Add questions

---
+++ js/questions.js
@@ -0,0 +1,265 @@
+const questions = [
+  {
+    question: 'Son aquellos medios y recursos que facilitan el proceso de enseñanza – aprendizaje:',
+    answer: [
+      'a) Alumno',
+      'b) Docente',
+      'c) Materiales didácticos'
+    ],
+    correct: 2
+  },
+  {
+    question: 'De que manera, Brunes y Olson señalan que se puede adquirir información:',
+    answer: [
+      'a) Directa e indirecta',
+      'b) A través del trabajo colaborativo',
+      'c) Recursos'
+    ],
+    correct: 0
+  },
+  {
+    question: '¿Qué autor nos dice que es un recurso de instrucción que proporciona al alumno una experiencia indirecta a la realidad?',
+    answer: [
+      'a) Meredith',
+      'b) Margarita Castañeda',
+      'c) Allen'
+    ],
+    correct: 1
+  },
+  {
+    question: 'Es una disciplina científica perteneciente al campo de la de la Dirección en la Educación',
+    answer: [
+      'a) Organización Escolar',
+      'b) Institución Educativa',
+      'c) Aprendizaje'
+    ],
+    correct: 0
+  },
+  {
+    question: 'La Organización Escolar Comprende, las realidades de una Educación',
+    answer: [
+      'a) Global',
+      'b) Básica y media superior',
+      'c) Formar y no formal'
+    ],
+    correct: 2
+  },
+  {
+    question: 'La Organización Escolar aborda realidades:',
+    answer: [
+      'a) Geográficas',
+      'b) Socios – Culturales',
+      'c) Religiosos'
+    ],
+    correct: 1
+  },
+  {
+    question: 'Los Materiales didácticos tienen una función de intermediario o mediador entre:',
+    answer: [
+      'a)  Maestros – alumnos',
+      'b) Enseñanza – Director',
+      'c) Escuela – Alumnos'
+    ],
+    correct: 0
+  },
+  {
+    question: '¿Qué es diseñar?',
+    answer: [
+      'a) Armar, constituir y elevar.',
+      'b) Crear, construir o edificar algo',
+      'c) Edificar, cubrir una necesidad o hacer.'
+    ],
+    correct: 1
+  },
+  {
+    question: '¿Qué se debe precisar para que el material didáctico sea eficiente?',
+    answer: [
+      'a) El papel del docente',
+      'b) El objetivo.',
+      'c) El estudio'
+    ],
+    correct: 1
+  },
+  {
+    question: '¿Qué autor recomienda que los materiales sean cuidadosamente diseñados, atractivos, sencillos y del tamaño del niño?',
+    answer: [
+      'a) Juan Amos Comenio',
+      'b) Jean Piaget',
+      'c) María Montessori'
+    ],
+    correct: 2
+  },
+  {
+    question: '¿Qué debe resultar evidente, con el diseño de los materiales didácticos?',
+    answer: [
+      'a) Los errores al igual que los éxitos del alumno',
+      'b) Los éxitos del profesor',
+      'c) Los avances y retrocesos de ambos'
+    ],
+    correct: 0
+  },
+  {
+    question: '¿Cuál es el principio de la intuición?',
+    answer: [
+      'a) La comunicación',
+      'b) Aproximar a la realidad',
+      'c) Acercar las ideas a los sentidos'
+    ],
+    correct: 2
+  },
+  {
+    question: '¿Para qué es necesario el material didáctico?',
+    answer: [
+      'a) Para tener una mejor oportunidad de aprendizaje',
+      'b) Para activar los sentidos',
+      'c) Para adquirir destrezas motoras.'
+    ],
+    correct: 0
+  },
+  {
+    question: '¿Una imagen, puede ser la realidad?',
+    answer: [
+      'a) No, solo es una representación',
+      'b) Si, porque tiene intención',
+      'c) No, es solo una imagen'
+    ],
+    correct: 0
+  },
+  {
+    question: 'Selecciona un recurso de comunicación actual, en el que las imágenes están en función de quien las mira:',
+    answer: [
+      'a) Las proyecciones',
+      'b) La radio',
+      'c) La televisión'
+    ],
+    correct: 2
+  },
+  {
+    question: '¿Por qué se dice que una imagen es polisémica?',
+    answer: [
+      'a) Porque tiene muchos textos',
+      'b) Porque implica diversos significados',
+      'c) Porque sus funciones son globales'
+    ],
+    correct: 1
+  },
+  {
+    question: '¿Qué es un lenguaje total?',
+    answer: [
+      'a) La ampliación del vocabulario',
+      'b) El uso de sonidos, palabras e imágenes',
+      'c) Los códigos de comunicación'
+    ],
+    correct: 1
+  },
+  {
+    question: '¿Cuál de los siguientes enunciados hace referencia a la característica de <<La imagen es significativa>>?',
+    answer: [
+      'a) “Se nos presenta cargada de una intención, con posibilidades de comunicar un mensaje”',
+      'b) “supone el dominio de los lenguajes por los que se expresa el mundo moderno”',
+      'c) “un soporte de comunicación visual que materializa un fragmento del universo”'
+    ],
+    correct: 0
+  },
+  {
+    question: '¿De qué manera es interpretada la realidad, a través de la imagen?',
+    answer: [
+      'a) De manera objetiva',
+      'b) De manera abstracta',
+      'c) De manera subjetiva'
+    ],
+    correct: 2
+  },
+  {
+    question: 'En los albores del siglo XXI es el recurso más utilizado.',
+    answer: [
+      'a) Papel',
+      'b) Lápiz',
+      'c) Borrador'
+    ],
+    correct: 0
+  },
+  {
+    question: 'Instrumento del que nos servimos para la construcción del conocimiento.',
+    answer: [
+      'a) Pizarrón',
+      'b) Medios didácticos',
+      'c) Estrategias y metodología'
+    ],
+    correct: 1
+  },
+  {
+    question: 'Según Moreno (2004), aprendizaje se entiende cómo.',
+    answer: [
+      'a) Material didáctico que se utilizan en la docencia',
+      'b) Paradigma técnico que desarrolla una teoría curricula',
+      'c) Actividad en donde el alumno adquiere una serie de conocimientos'
+    ],
+    correct: 2
+  },
+  {
+    question: 'Formas de comunicación de los medios.',
+    answer: [
+      'a) Lenguaje visual, visual, escrito y combinación de lenguajes',
+      'b) Lenguaje verbal, sonoro y oral.',
+      'c) Lenguaje memorístico, temporal y libre.'
+    ],
+    correct: 0
+  },
+  {
+    question: 'Los materiales didácticos se clasifican en.',
+    answer: [
+      'a) Diversificados y pluridimensionales',
+      'b) Cerrados y abiertos',
+      'c) Uniformes y acumulativos'
+    ],
+    correct: 1
+  },
+  {
+    question: 'Es uno de los aspectos que prevalecen en los materiales didácticos cerrados.',
+    answer: [
+      'a) Adaptables a la realidad',
+      'b) Aprendizaje memorístico',
+      'c) Técnicas blandas'
+    ],
+    correct: 1
+  },
+  {
+    question: 'Son medios de apoyo a la comunicación oral.',
+    answer: [
+      'a) Libros, guías, revistas',
+      'b) Reglas, papeles, colores',
+      'c) Carteles, videos y pizarrón'
+    ],
+    correct: 2
+  },
+  {
+    question: 'Son ejemplos de medios de información continua.',
+    answer: [
+      'a) Páginas web, correo electrónico y videoconferencias',
+      'b) Computadora, libros y recursos',
+      'c) Mensajes, textos y folletos'
+    ],
+    correct: 0
+  },
+  {
+    question: 'Es un medio tradicional que es fundamentalmente visual y permite la inclusión de dibujos que completan los textos.',
+    answer: [
+      'a) Internet',
+      'b) La pizarra',
+      'c) Los textos'
+    ],
+    correct: 1
+  },
+  {
+    question: 'Los medios de enseñanza tiene como misión fundamental.',
+    answer: [
+      'a) Clasificar el conocimiento',
+      'b) Planificar las clases',
+      'c) Facilitar el aprendizaje de los alumnos'
+    ],
+    correct: 2
+  }
+]
+
+export default questions
